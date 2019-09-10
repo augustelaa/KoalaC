d51054dc9e9827909ae7dc7e1d2347ae16555e9c
Augusto <augusto_conceicao@hotmail.com>
2019-09-10 19:38:47 -0300

    Ajustes

---
+++ Interface/src/Compiladores_Backend/Constants.java
@@ -0,0 +1,46 @@
+public interface Constants extends ScannerConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_palavra = 2;
+    int t_identificadorInt = 3;
+    int t_identificadorFloat = 4;
+    int t_identificadorString = 5;
+    int t_identificadorBool = 6;
+    int t_identificadorComposto = 7;
+    int t_constanteInt = 8;
+    int t_constanteFloat = 9;
+    int t_constanteString = 10;
+    int t_input = 11;
+    int t_isfalsedo = 12;
+    int t_istruedo = 13;
+    int t_main = 14;
+    int t_output = 15;
+    int t_true = 16;
+    int t_types = 17;
+    int t_while = 18;
+    int t_TOKEN_19 = 19; //"."
+    int t_TOKEN_20 = 20; //","
+    int t_TOKEN_21 = 21; //":"
+    int t_TOKEN_22 = 22; //";"
+    int t_TOKEN_23 = 23; //"["
+    int t_TOKEN_24 = 24; //"]"
+    int t_TOKEN_25 = 25; //"("
+    int t_TOKEN_26 = 26; //")"
+    int t_TOKEN_27 = 27; //"+"
+    int t_TOKEN_28 = 28; //"-"
+    int t_TOKEN_29 = 29; //"*"
+    int t_TOKEN_30 = 30; //"/"
+    int t_TOKEN_31 = 31; //"="
+    int t_TOKEN_32 = 32; //"=="
+    int t_TOKEN_33 = 33; //"!="
+    int t_TOKEN_34 = 34; //"<"
+    int t_TOKEN_35 = 35; //"<="
+    int t_TOKEN_36 = 36; //">"
+    int t_TOKEN_37 = 37; //">="
+    int t_TOKEN_38 = 38; //"!"
+    int t_TOKEN_39 = 39; //"&&"
+    int t_TOKEN_40 = 40; //"||"
+
+}
