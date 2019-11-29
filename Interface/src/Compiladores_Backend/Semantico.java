f2dd6793a27e2dc421cadc17fc86f78a1b1b1f1e
Augusto <augusto_conceicao@hotmail.com>
2019-11-29 19:23:26 -0300

    Adição da base do semantico.

--- Interface/src/Compiladores_Backend/Semantico.java
+++ Interface/src/Compiladores_Backend/Semantico.java
@@ -1,8 +1,25 @@
 package Compiladores_Backend;
+
+import java.util.Stack;
+
 public class Semantico implements Constants
 {
+    StringBuilder codigoGerado = new StringBuilder();
+    Stack<Tipos> pilha = new Stack<Tipos>();
+
     public void executeAction(int action, Token token)	throws SemanticError
     {
-        System.out.println("Ação #"+action+", Token: "+token);
+        switch(action) {
+            case 15:
+                codigoGerado.append(".assembly extern mscorlib ...");
+                break;
+            case 5:
+                pilha.push(Tipos.t_int64);
+                codigoGerado.append("ldc.i8 ");
+                codigoGerado.append(token.getLexeme());
+                codigoGerado.append("\n conv.r8");
+                break;
+        }
     }	
 }
+
---
+++ Interface/src/Compiladores_Backend/Tipos.java
@@ -0,0 +1,5 @@
+package Compiladores_Backend;
+
+public enum Tipos {
+    t_int64, t_float64, t_bool, t_double;
+}
