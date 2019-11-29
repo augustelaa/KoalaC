36d98701381ac964f6bea32eb6f3ddfd4057ca31
Marcelo <marcelo_4940@hotmail.com>
2019-11-29 19:52:46 -0300

    Vai

--- Interface/src/Compiladores_Backend/Semantico.java
+++ Interface/src/Compiladores_Backend/Semantico.java
@@ -2,16 +2,17 @@ package Compiladores_Backend;
 
 import java.util.Stack;
 
-public class Semantico implements Constants
-{
+public class Semantico implements Constants {
+
     StringBuilder codigoGerado = new StringBuilder();
     Stack<Tipos> pilha = new Stack<Tipos>();
 
-    public void executeAction(int action, Token token)	throws SemanticError
-    {
-        switch(action) {
+    public void executeAction(int action, Token token) throws SemanticError {
+        Tipos tipo1;
+        Tipos tipo2;
+        switch (action) {
             case 15:
-                codigoGerado.append(".assembly extern mscorlib ...");
+                codigoGerado.append(".assembly extern mscorlib ..."); //COmpletar
                 break;
             case 5:
                 pilha.push(Tipos.t_int64);
@@ -19,7 +20,29 @@ public class Semantico implements Constants
                 codigoGerado.append(token.getLexeme());
                 codigoGerado.append("\n conv.r8");
                 break;
+            case 1: 
+                Tipos tipo1 = pilha.pop();
+                Tipos tipo2 = pilha.pop();
+                
+                if (tipo1 == Tipos.t_float64 || tipo2 == Tipos.t_float64) { 
+                    pilha.push(Tipos.t_float64);
+                } else {
+                    pilha.push(Tipos.t_int64);
+                }
+                codigoGerado.append("add");
+                break;
+                
+                case 2:
+                Tipos tipo1 = pilha.pop();
+                Tipos tipo2 = pilha.pop();
+                
+                if (tipo1 == Tipos.t_float64 || tipo2 == Tipos.t_float64) { 
+                    pilha.push(Tipos.t_float64);
+                } else {
+                    pilha.push(Tipos.t_int64);
+                }
+                codigoGerado.append("add");
+                break;    
         }
-    }	
+    }
 }
-
